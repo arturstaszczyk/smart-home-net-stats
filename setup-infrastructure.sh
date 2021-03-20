f3f14de6235297a6afe9aa03fc8253e46071f4ff
Artur <sforstasiu+github@gmail.com>
2021-03-20 23:22:51 +0100

    whole stuff running

--- setup-infrastructure.sh
+++ setup-infrastructure.sh
@@ -3,6 +3,7 @@
 docker stop grafana influxdb
 docker rm grafana influxdb
 
+docker network create smart-home
 docker run -d -p 3000:3000 --net=smart-home --network-alias=grafana --name=grafana grafana/grafana
 docker run -d -p 8086:8086 -v ~/Project/influxdb:/var/lib/influxdb --net=smart-home --network-alias=influxdb --name=influxdb smart-home-influx
 docker ps
