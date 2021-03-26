e02714b14461b70347fdf84e3596266cf5a25aaa
Artur <sforstasiu+github@gmail.com>
2021-03-26 23:09:05 +0100

    Update .env file loc

--- Service/service-run.sh
+++ Service/service-run.sh
@@ -3,4 +3,4 @@
 docker stop smart-home-service
 docker rm smart-home-service
 
-docker run -d --env-file .env -v /var/lib/smart-home/service:/var/log --name=smart-home-service smart-home-service
+docker run -d --env-file /var/lib/smart-home/service/.env -v /var/lib/smart-home/service:/var/log --name=smart-home-service smart-home-service
