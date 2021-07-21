42650509a65752f9e6ef435917deaa87d0bc57f0
Artur <arturstaszczyk@users.noreply.github.com>
2021-07-21 23:42:54 +0200

    Update service-build.sh

--- Service/service-build.sh
+++ Service/service-build.sh
@@ -1,1 +1,1 @@
-docker build --no-cache --tag smart-home-service --file Dockerfile.service .
+docker build --no-cache --tag smart-home-service --build-arg SPEED_TEST_MUNUTE=${1-7} --file Dockerfile.service .
