a670a6d0bbdfa16afadfba583152112fb0e4cad1
Artur <sforstasiu@gmail.com>
2023-04-15 14:04:48 +0100

    Updated container script to not repeat entry in crontab

--- Service/containerScripts/init-container-script.sh
+++ Service/containerScripts/init-container-script.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+echo "Printing content of env variables of Docker container"
 printenv > env.docker.sh
 cat env.docker.sh
 
 # Add crontab file in the cron directory
+echo "Adding cron config for infrequent command"
+rm /var/smart-home/containerScripts/cron-job-smart-home.copy
+cp /var/smart-home/containerScripts/cron-job-smart-home /var/smart-home/containerScripts/cron-job-smart-home.copy
+echo $SPEED_TEST_MINUTE" * * * * . /var/smart-home/env.docker.sh; /var/smart-home/containerScripts/run-script.sh infrequent >> /var/log/cron2.log 2>&1" >> /var/smart-home/containerScripts/cron-job-smart-home.copy
+echo "" >> /var/smart-home/containerScripts/cron-job-smart-home.copy
+cp ./containerScripts/cron-job-smart-home.copy /etc/cron.d
 
-echo $SPEED_TEST_MINUTE" * * * * . /var/smart-home/env.docker.sh; /var/smart-home/containerScripts/run-script.sh infrequent >> /var/log/cron2.log 2>&1" >> /var/smart-home/containerScripts/cron-job-smart-home
-echo "" >> /var/smart-home/containerScripts/cron-job-smart-home
-cp ./containerScripts/cron-job-smart-home /etc/cron.d
-
+echo "Printing cron config"
 cat /etc/cron.d/cron-job-smart-home
+echo "Printing cron.d content"
 ls /etc/cron.d
 
 # Give execution rights on the cron job
@@ -19,7 +24,7 @@ chmod 0644 /etc/cron.d/cron-job-smart-home
 crontab /etc/cron.d/cron-job-smart-home
 
 
-printenv > .env 
+printenv > .env s
 touch /var/log/cron.log 
 echo "" > /var/log/cron.log 
 cron
