dd3559bcc4762b2cd2bf8571ba6eee4ea3c2d247
Artur <sforstasiu@gmail.com>
2023-04-16 10:30:09 +0200

    Updated infrastructure init script

--- Infrastructure/infrastructure-run.sh
+++ Infrastructure/infrastructure-run.sh
@@ -8,9 +8,16 @@ mkdir -p /var/lib/smart-home/grafana
 
 sudo chown 472:472 /var/lib/smart-home/grafana
 
+echo "Creating smart-home network"
 docker network create smart-home
-docker run --restart unless-stopped -d -p 3000:3000 -v /var/lib/smart-home/grafana:/var/lib/grafana --net=smart-home --network-alias=grafana --name=grafana -e "GF_AUTH_DISABLE_LOGIN_FORM=true" -e "GF_AUTH_ANONYMOUS_ENABLED=true" -e "GF_AUTH_ANONYMOUS_ORG_ROLE=Admin" -e "GF_SECURITY_ALLOW_EMBEDDING=true" grafana/grafana
+echo "Running influx docker"
 docker run --restart unless-stopped -d -p 8086:8086 -v /var/lib/smart-home/influx:/var/lib/influxdb --net=smart-home --network-alias=influxdb --name=influxdb smart-home-influx
-sleep 10
+echo "Running grafana docker"
+docker run --restart unless-stopped -d -p 3000:3000 -v /var/lib/smart-home/grafana:/var/lib/grafana --net=smart-home --network-alias=grafana --name=grafana -e "GF_AUTH_DISABLE_LOGIN_FORM=true" -e "GF_AUTH_ANONYMOUS_ENABLED=true" -e "GF_AUTH_ANONYMOUS_ORG_ROLE=Admin" -e "GF_SECURITY_ALLOW_EMBEDDING=true" grafana/grafana
+echo "Sleeping 20"
+sleep 20
+echo "Ruinning init influx"
 docker run --net=smart-home smart-home-init-influx
+
+echo "Inspecting running docker processes"
 docker ps
